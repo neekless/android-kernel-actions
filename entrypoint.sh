48dbfc8e1092ba08b985ee60485cd88c2448d098
Nicholas <22426822+neekless@users.noreply.github.com>
2021-07-09 10:58:41 +0800

    Add liblz4

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ apt update && apt upgrade -y
 msg "Installing essential packages..."
 apt install -y --no-install-recommends git make bc bison openssl \
     curl zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget python3 python2 \
-    device-tree-compiler ca-certificates xz-utils
+    device-tree-compiler ca-certificates xz-utils liblz4-tool
 ln -sf /usr/bin/python3 /usr/bin/python
 set_output hash "$(cd "$kernel_path" && git rev-parse HEAD || exit 127)"
 msg "Installing toolchain..."
